c45492dd8247dd59b951ea321d213c6e6d3098bc
Silva1997 <Lucaseugeniofernando@gmail.com>
2024-05-16 08:48:59 -0300

    Ap

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/MeuPP',
-    build: {
-        chunkSizeWarningLimit: 1600,
-    },
+
+
 })
