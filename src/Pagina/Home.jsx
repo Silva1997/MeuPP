b02a4b266c6f59612854393d89ee2284240c5e23
Silva1997 <Lucaseugeniofernando@gmail.com>
2024-05-17 07:47:01 -0300

    bom

--- src/Pagina/Home.jsx
+++ src/Pagina/Home.jsx
@@ -8,14 +8,30 @@ import '../style/Estilo.css'; // Importando o arquivo CSS onde as classes serão
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import SearchIcon from '@mui/icons-material/Search';
 const App = () => {
+   const [pesquisar,setPesquisar] = React.useState();
   const onChange = (currentSlide) => {
     console.log(currentSlide);
   };
 
+  const handlePesquisa = (e) => {
+    
+    if(e.key ==='Enter'){
+      console.log(pesquisar);
+    }
+    else{
+      return ''
+    }
+  };
+
+
   return (
     <>
     <div className='INput'  >
-    <Input size="large" className='inputs'  placeholder="Procurar itens perdidos" prefix={<SearchIcon color='primary' />} />
+    <Input size="large" className='inputs'  placeholder="Procurar itens perdidos" prefix={<SearchIcon color='primary' />}
+    value={pesquisar}
+    onChange={(e)=>setPesquisar(e.target.value)}
+    onKeyDown={handlePesquisa}
+    />
     </div>
     {/*  */}
       <div className="container">
@@ -68,8 +84,32 @@ const App = () => {
           <img className="card-image" src={Photo} alt="Ac" />
         </Card>
       </div>
+
+      <Rodape/>
     </>
   );
 };
 
 export default App;
+
+
+function Rodape(){
+  return(
+    <footer className='ft'>
+
+<div style={{borderRight:" 2px solid#b152dd" }} >
+<p>Encontre seus itens</p>
+<p>perdidos com facilidade</p>
+</div>
+<div style={{borderRight:" 2px solid#b152dd" }} >
+<p>Encontre seus itens</p>
+<p>perdidos com facilidade</p>
+</div>
+<div style={{borderRight:" 2px solid#b152dd" }} >
+<p>Encontre seus itens</p>
+<p>perdidos com facilidade</p>
+</div>
+
+    </footer>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './Route/Rotas'
+// import './index.css'
+// import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// const route = createBrowserRouter([{
+//   path:'/',
+//   element:<div> Hello </div>
+// }])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={route}/>   
+//  </React.StrictMode>,
+// )
+
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App'
+import "./index.css";
+// import Root from "./Route/Rotas";
+// import ErrorPage  from "./Route/Erro";
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root/>,
+//     errorElement: <ErrorPage />,
+
+//   },
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App/>
+    {/* <RouterProvider router={router} /> */}
+  </React.StrictMode>
+);
+
